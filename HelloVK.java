62050df357f345e2d6fdc4d1d3ecde1801c381e8
Vishwas K <kvishwas796@gmail.com>
2026-03-19 23:08:51 +0530

    Add files via upload

---
+++ HelloVK.java
@@ -0,0 +1,8 @@
+package Basics;
+
+public class HelloVK {
+    static void main() {
+        System.out.println("Hello VK");
+        System.out.print("Be positive");
+    }
+}
---
+++ PrintingNumbers.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class PrintingNumbers {
+    static void main() {
+        System.out.println("4+3");
+        System.out.println(4);
+        System.out.println(4+3);
+    }
+}
