dc038f4cb6ffcae4afbcaa602d3d1dddded4b640
Vishwas K <kvishwas796@gmail.com>
2026-03-21 22:18:58 +0530

    Add files via upload

---
+++ percentageOf5sub.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class percentageOf5sub {
+    static void main() {
+        double x1=100;
+        double x2=92;
+        double x3=91;
+        double x4=86;
+        double x5=77;
+        double v=(x1+x2+x3+x4+x5)/5;
+        System.out.println(v);
+    }
+}
