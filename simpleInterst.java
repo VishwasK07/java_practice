1bef63d0f341e4982e068eee2e59a4c9075da7dc
Vishwas K <kvishwas796@gmail.com>
2026-03-22 18:31:05 +0530

    Add files via upload

---
+++ simpleInterst.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class simpleInterst {
+    static void main() {
+        double principal = 1000;
+        double rate = 22;
+        double time = 5;
+        double si=(principal*rate*time)/100;
+        System.out.println(si);
+    }
+}
