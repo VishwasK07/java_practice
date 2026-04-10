764d33af057ddc18706cb0e26bf74db6881b5d14
Vishwas K <kvishwas796@gmail.com>
2026-04-11 00:39:42 +0530

    Add files via upload

---
+++ decrement.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class decrement {
+    static void main() {
+        for(int i = 99;i > 0;i -= 4){
+            System.out.print(i+" ");
+        }
+
+    }
+}
+//int a=99;
+//for(int i=1;i<=25;i++){
+//    sout(a+" ");
+//    a-=4;
+//        }
